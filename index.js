41654803d7e9087bd9ef2b5ff7b49b9df9fa3b96
Aakash Sigdel <aakash.sigdel@gmail.com>
2019-06-30 09:49:21 +0200

    depend on local copy of babel-plugin-dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = () => ({
   plugins: [
-    [require('babel-plugin-dotenv'), {
+    [require('./babel-plugin-dotenv'), {
       replacedModuleName: 'react-native-dotenv',
       configDir: path.resolve(__dirname, "../../")
     }],
